9818e909e22085a27adcedd0f77c01d4d5829f63
thumbsu <uumj222@gmail.com>
2026-03-27 14:57:19 +0900

    patchdeck 0.1.3

--- Formula/patchdeck.rb
+++ Formula/patchdeck.rb
@@ -1,9 +1,9 @@
 class Patchdeck < Formula
   desc "Review-first console for multi-worktree Git workflows"
   homepage "https://github.com/thumbsu/patchdeck"
-  url "https://github.com/thumbsu/patchdeck/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "12ec8c9b78b200a99f45923c67b3a1c65054acee8dbf8f094b5e2b25eeb21fd4"
-  version "0.1.2"
+  url "https://github.com/thumbsu/patchdeck/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "d4a2a593397972fea5551b41d18e452705d49da20fc3c8da5447e87856fa3f7e"
+  version "0.1.3"
 
   depends_on "go" => :build
 
